605031c238ea3263ae0356a662a973d15c359013
Ktripathi2611 <tripathikushal522@gmail.com>
2024-07-18 15:00:13 +0530

    v1.0 starting with frontEnd

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+    <div>
+      <h1 className="font-bold flex justify-center items-center text-3xl text-blue-800">
+        To-Do list using MERN
+      </h1>
+    </div>
+    
+    </>
+  );
+}
